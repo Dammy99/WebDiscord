30546b8e4a53d549685e07414fb1b59d618ce908
Dammy99 <deslyly.prod@gmail.com>
2023-08-27 14:43:41 +0300

    Deleted unused file

--- src/config/reduxConfig.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import VideosReducer from "../reducers/VideosReducer";
-
-const rootReducer = combineReducers({
-  VideosReducer: VideosReducer,
-});
-
-export const store = configureStore({ reducer: rootReducer });
